f0c55000104956b3928b58c5cdb811590b43b9ee
Konstantin Spirin <konstmsu@gmail.com>
2024-10-25 02:17:32 +0800

    restore file logging

--- printer_keeper/__main__.py
+++ printer_keeper/__main__.py
@@ -7,6 +7,10 @@ from printer_keeper.fortune import logger, main
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler("printer_keeper.log", encoding="utf8"),
+        logging.StreamHandler(),
+    ],
 )
 
 
